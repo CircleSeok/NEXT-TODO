a73e0f0e0f8106a44838c6ff2a4b777341cdfd81
Wonseok <dnjstjr970302@gmail.com>
2024-11-28 21:42:02 +0900

    next ui table 적용, Todo 타입 설정, todos데이터 fetch

---
+++ app/counter/page.tsx
@@ -0,0 +1,19 @@
+import { Counter } from '@/components/counter';
+import { title } from '@/components/primitives';
+
+async function getInitialCount() {
+  // console.log('getInitialCount called');
+  await new Promise((f) => setTimeout(f, 3000));
+  return 10;
+}
+
+export default async function CounterPage() {
+  const fetchedInitialCount = await getInitialCount();
+
+  return (
+    <div className='flex flex-col space-y-16'>
+      <h1 className={title()}>Counter</h1>
+      <Counter initialCount={fetchedInitialCount} />
+    </div>
+  );
+}
---
+++ app/todos/page.tsx
@@ -0,0 +1,25 @@
+import { title } from '@/components/primitives';
+import TodosTable from '@/components/todos-table';
+import { fetchTodos } from '@/data/firestore';
+
+async function fetchTodosApiCall() {
+  // console.log('fetchTodosApiCall called');
+  const res = await fetch(`${process.env.BASE_URL}/api/todos`);
+
+  // if (!res.ok) {
+  //   throw new Error('Failed to fetch data');
+  // }
+  return res.json();
+}
+
+export default async function TodosPage() {
+  const response = await fetchTodosApiCall();
+
+  return (
+    <div className='flex flex-col space-y-8'>
+      <h1 className={title()}>Todos</h1>
+      <TodosTable todos={response.data ?? []} />
+      {/* <TodosTable todos={[]} /> */}
+    </div>
+  );
+}
